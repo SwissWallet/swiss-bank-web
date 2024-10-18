1cd83122e53d000a749ed45ea5b6b40c0395e16f
Brennon <brennonmeireles7@gmail.com>
2024-10-17 22:09:58 -0300

    feat: created style to input login component

---
+++ src/components/input-login/input-style.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const StyledInputLogin = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    gap: 60px;
+    padding: 100px;
+
+    width: 60%;
+
+    h2 {
+        font-family: 'Inter', sans-serif;
+        font-weight: 400;
+        font-size: 32px;
+
+        color: #FFFFFF;
+    }
+
+    input {
+        border: 1px solid #A7A7A7;
+        color: #c50000;
+        font-size: 20px;
+        padding: 30px 25px;
+        border-radius: 5px;
+        background-color: transparent;
+    }
+
+    input::placeholder {
+        color: #c50000;
+    }
+
+    a {
+        text-align: end;
+
+        font-family: 'Inter', sans-serif;
+        font-weight: 400;
+        font-size: 20px;
+
+        color: #FFFFFF;
+    }
+
+    button {
+        display: flex;
+        justify-content: center;
+        border: none;
+        border-radius: 5px;
+        background-color: #C50000;
+        color: #fff;
+        font-size: 24px;
+        padding: 30px 250px;
+    }
+`
