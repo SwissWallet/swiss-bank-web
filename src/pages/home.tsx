abc25eb6fbea27b62e3080ec0a78be0f5d9545c3
Brennon <brennonmeireles7@gmail.com>
2024-10-23 21:42:41 -0300

    feat: created home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,18 @@
+import AccoutBalance from "../components/accout-balance/account-balance";
+import ExtractArea from "../components/extract-area/extract-area";
+import PerfilHeader from "../components/perfil-header/perfil-header";
+import Sidebar from "../components/sidebar/sidebar";
+
+export default function Home(){
+    return(
+        <div style={{display:"flex", backgroundColor:"#1B1B1B"}}>
+            <Sidebar />
+            <div style={{width: "100%", padding:"30px 60px", display:"flex", flexDirection: "column", gap:"30px", height:"100vh"}}>
+                <PerfilHeader />
+                <AccoutBalance />
+                <ExtractArea />
+            </div>
+        </div>
+
+    )
+}
