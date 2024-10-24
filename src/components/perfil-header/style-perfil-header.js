25c284a4e8179c641ce0a97a552eba8cc84827b2
Brennon <brennonmeireles7@gmail.com>
2024-10-23 21:45:43 -0300

    feat: created style to perfil header component

---
+++ src/components/perfil-header/style-perfil-header.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyleHeader = styled.div`
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid #a7a7a7;
+    padding: 20px;
+    border-radius: 5px;
+
+    p{
+        color: #d6d6d6;
+        font-family: "Roboto", sans-serif;
+    }
+
+    div {
+        display: flex;
+        justify-content: space-between;
+        gap: 20px;
+    }
+
+    article{
+        border: 1px solid #a7a7a7;
+        padding: 20px;
+        border-radius: 5px;
+
+    }
+
+
+`
