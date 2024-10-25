60ec79772b2b6de1a84296b1eb124ddafc7aeca6
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-25 19:40:03 -0300

    feat: get user balance at section balange at home

--- src/components/accout-balance/account-balance.tsx
+++ src/components/accout-balance/account-balance.tsx
@@ -1,13 +1,18 @@
+import { useSelector } from "react-redux";
 import HandMoney from "../../assets/hand-money.png"
 
 import {StyleBalance} from "./style-balance.js"
+import { RootState } from "../../store.js";
 
 export default function AccoutBalance(){
+
+    const balance = useSelector((state: RootState) => state.account.value.balance);
+
     return(
         <StyleBalance>
             <div>
                 <p>Valor total em conta</p>
-                <h1>R$<span>1700</span>,00</h1>
+                <h1>R$<span>{balance}</span>,00</h1>
             </div>
             <section>
                 <p>PIX</p>
