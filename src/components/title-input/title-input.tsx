3508cde9f040161cce53846ebe1df9bd5d0d48ae
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-28 20:11:14 -0300

    fix: remove redux

--- src/components/title-input/title-input.tsx
+++ src/components/title-input/title-input.tsx
@@ -1,14 +1,10 @@
 import { StyledTitleInput } from "../title-input/title-input-style.js"
-import { useSelector } from "react-redux"
-import { RootState } from "../../store.js"
 import { useState } from "react"
 import { Eye, EyeClosed } from "lucide-react"
 
 export default function TitleInput(){
 
     const [ isShowBalance, setIsShowBalance ] = useState<boolean>(true);
-
-    const balance = useSelector((state: RootState) => state.account.value.balance);
 
     return(
         <StyledTitleInput>
@@ -18,7 +14,7 @@ export default function TitleInput(){
             </div>
             <article>
                 <h4>Saldo em conta</h4>
-                <p>$ {balance}</p>
+                <p>$ {"30"}</p>
                 {isShowBalance ? (
                     <button onClick={() => setIsShowBalance(false)}>
                         <Eye />
