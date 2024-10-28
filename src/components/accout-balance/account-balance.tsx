a73c70d2028c8f4828eaa8c85ad21ff728b7e9fd
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-28 20:16:16 -0300

    feat: showing balance limit

--- src/components/accout-balance/account-balance.tsx
+++ src/components/accout-balance/account-balance.tsx
@@ -1,16 +1,19 @@
+import { useContext } from "react";
 import HandMoney from "../../assets/hand-money.png"
 
 import {StyleBalance} from "./style-balance.js"
 import { Link } from "react-router-dom";
+import { UserContext } from "../../context/user/index.js";
 
 export default function AccoutBalance(){
 
+    const { card } = useContext(UserContext);
 
     return(
         <StyleBalance>
             <div>
                 <p>Valor total em conta</p>
-                <h1>R$<span>{}</span>,00</h1>
+                <h1>R$<span>{card?.cardLimit}</span>,00</h1>
             </div>
             <section>
                 <p>PIX</p>
