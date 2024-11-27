74d4eafd43492251562d0e6d7bbc1236d29d4b83
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-11-27 20:53:41 -0300

    fix: replaced styled component to tailwind sidebar

--- src/components/perfil-header/perfil-header.tsx
+++ src/components/perfil-header/perfil-header.tsx
@@ -1,19 +1,15 @@
 import { useContext } from "react"
-import Sino from "../../assets/sino.png"
 import { UserContext } from "../../context/user/index.js"
+import { BellRing } from "lucide-react";
 export default function PerfilHeader(){
 
     const { user } = useContext(UserContext);
     
     return(
         <div className="flex justify-between border-2 border-[#A7A7A7] p-5 rounded-md">
-            <div className="flex justify-between gap-5 w-full">
-                <div className="flex flex-col justify-between">
-                    <p className="text-[#D6D6D6] font-sans font-light text-2xl">Bem-vindo de volta,<br/>{user?.name}</p>
-                </div>
-            </div>
-            <article className="border-2 border-[#A7A7A7] p-5 rounded-md transition-all duration-150 ease-in-out cursor-pointer hover:scale-[1.05]">
-                <img src={Sino} alt="" />
+            <p className="text-[#D6D6D6] font-light text-2xl">Bem-vindo de volta,<br/><strong>{user!.name}</strong></p>
+            <article className="border-2 border-[#A7A7A7] text-[#a7a7a7] hover:text-[#c50000] hover:bg-[#a7a7a7] p-5 rounded-md transition-all duration-150 ease-in-out cursor-pointer hover:scale-[1.05]">
+                <BellRing className=""/>
             </article>
         </div>
     )
